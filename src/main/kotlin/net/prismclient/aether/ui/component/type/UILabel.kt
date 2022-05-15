0e450e9ad5b4edd0dcdbca10090e7b005990ee9e
sen <102120097+senxd@users.noreply.github.com>
2022-05-15 10:57:59 -0700

    Update UILabel.kt

--- src/main/kotlin/net/prismclient/aether/ui/component/type/UILabel.kt
+++ src/main/kotlin/net/prismclient/aether/ui/component/type/UILabel.kt
@@ -3,8 +3,14 @@ package net.prismclient.aether.ui.component.type
 import net.prismclient.aether.ui.component.UIComponent
 import net.prismclient.aether.ui.style.UIStyleSheet
 
-class UILabel(style: String) : UIComponent<UIStyleSheet>(style) {
+/**
+ * [UILabel] is a component which draws a label, or string on screen.
+ *
+ * @author sen
+ * @since 5/15/2022
+ */
+class UILabel(var text: String, style: String) : UIComponent<UIStyleSheet>(style) {
     override fun renderComponent() {
-
+        style.font?.render(text)
     }
 }
