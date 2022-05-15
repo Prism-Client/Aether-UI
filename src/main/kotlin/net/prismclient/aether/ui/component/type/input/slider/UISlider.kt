0d06781d3b916e79dbac8dfeb29dc896321f6c9c
sen <102120097+senxd@users.noreply.github.com>
2022-05-14 22:05:25 -0700

    Cap at initial set for sliders

--- src/main/kotlin/net/prismclient/aether/ui/component/type/input/slider/UISlider.kt
+++ src/main/kotlin/net/prismclient/aether/ui/component/type/input/slider/UISlider.kt
@@ -1,5 +1,6 @@
 package net.prismclient.aether.ui.component.type.input.slider
 
+import net.prismclient.aether.UICore
 import net.prismclient.aether.ui.component.UIComponent
 import kotlin.math.roundToInt
 
@@ -10,8 +11,9 @@ import kotlin.math.roundToInt
  * @since 5/13/2022
  */
 open class UISlider(var value: Float, var min: Float, var max: Float, var step: Float, style: String) : UIComponent<UISliderSheet>(style) {
-    protected var normalizedValue = value / (max - min)
-
+    protected var normalizedValue = (value / (max - min))
+            .coerceAtLeast(0f)
+            .coerceAtMost(1f)
     protected var offsetX = 0f
     protected var selected = false
 
@@ -28,12 +30,10 @@ open class UISlider(var value: Float, var min: Float, var max: Float, var step: 
     override fun mouseMoved(mouseX: Float, mouseY: Float) {
         super.mouseMoved(mouseX, mouseY)
         if (selected) { // I got lazy
-            normalizedValue = (((mouseX - offsetX - style.sliderControl.cachedX)) / (relWidth - style.sliderControl.cachedWidth))
+            normalizedValue = (((UICore.mouseX - offsetX - style.sliderControl.cachedX)) / (relWidth - style.sliderControl.cachedWidth))
                     .coerceAtLeast(0f)
                     .coerceAtMost(1f)
-            value = (max - min) * normalizedValue + min
-            value = (value / step).roundToInt() * step
-            normalizedValue = value / (max - min)
+            updateSlider()
         }
     }
 
@@ -46,7 +46,7 @@ open class UISlider(var value: Float, var min: Float, var max: Float, var step: 
                         style.sliderControl.cachedWidth,
                         style.sliderControl.cachedHeight
         )) {
-            offsetX = mouseX - (style.sliderControl.cachedX + style.sliderControl.offsetX)
+            offsetX = UICore.mouseX - (style.sliderControl.cachedX + style.sliderControl.offsetX)
             selected = true
         }
     }
@@ -54,5 +54,11 @@ open class UISlider(var value: Float, var min: Float, var max: Float, var step: 
     override fun mouseReleased(mouseX: Float, mouseY: Float) {
         super.mouseReleased(mouseX, mouseY)
         selected = false
+    }
+
+    fun updateSlider() {
+        value = (max - min) * normalizedValue + min
+        value = (value / step).roundToInt() * step
+        normalizedValue = value / (max - min)
     }
 }
