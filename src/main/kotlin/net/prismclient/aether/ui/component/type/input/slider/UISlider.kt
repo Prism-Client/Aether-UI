99e8df5d8535b0fac3aa03156858673c24aeaf39
sen <102120097+senxd@users.noreply.github.com>
2022-05-18 22:22:41 -0700

    Update UISlider.kt

--- src/main/kotlin/net/prismclient/aether/ui/component/type/input/slider/UISlider.kt
+++ src/main/kotlin/net/prismclient/aether/ui/component/type/input/slider/UISlider.kt
@@ -6,16 +6,26 @@ import java.util.function.Consumer
 import kotlin.math.roundToInt
 
 /**
- * [UISlider]
+ * [UISlider] is a form of input which allows the user to select a value
+ * from a range. The default slider [UISlider], supports a minimum, maximum,
+ * and step to control the value. If a range is preferred, please refer to
+ * [UIRangeSlider] instead.
  *
  * @author sen
  * @since 5/13/2022
+ *
+ * @param value The value of the slider
+ * @param min The minimum value that the slider should be
+ * @param max The maximum value that the slider should be
+ * @param step The value which the slider should step by
  */
 open class UISlider(value: Float, var min: Float, var max: Float, var step: Float, style: String) : UIComponent<UISliderSheet>(style) {
     var value: Float = value
         set(value) {
-            field = value
-            valueListeners?.forEach { it.accept(this) }
+            val prev = this.value
+            field = ((value / step).roundToInt() * step).coerceAtLeast(min).coerceAtMost(max)
+            if (prev != field)
+                valueListeners?.forEach { it.accept(this) }
         }
 
     protected var normalizedValue = (value / (max - min))
@@ -63,7 +73,6 @@ open class UISlider(value: Float, var min: Float, var max: Float, var step: Floa
 
     fun updateSlider() {
         value = (max - min) * normalizedValue + min
-        value = (value / step).roundToInt() * step
         normalizedValue = value / (max - min)
     }
 
