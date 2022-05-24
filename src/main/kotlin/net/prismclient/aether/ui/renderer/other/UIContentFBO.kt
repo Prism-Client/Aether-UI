bc34d4acfacd420638d9a653254ef7549cafd8bf
sen <102120097+senxd@users.noreply.github.com>
2022-05-24 09:25:26 -0700

    Update UIContentFBO.kt

--- src/main/kotlin/net/prismclient/aether/ui/renderer/other/UIContentFBO.kt
+++ src/main/kotlin/net/prismclient/aether/ui/renderer/other/UIContentFBO.kt
@@ -1,8 +1,10 @@
 package net.prismclient.aether.ui.renderer.other
 
+import net.prismclient.aether.ui.component.type.layout.UIFrame
+
 /**
- * [UIContentFBO] is a data class which holds the id, width, and height of
- * a created FBO. [UIContentFBO] is usually created when a [UIFrame] is made
+ * [UIContentFBO] represents an FBO object. They are general created
+ * when a [UIFrame] is created
  *
  * @author sen
  * @since 5/1/2022
@@ -11,5 +13,7 @@ class UIContentFBO(
     val id: Int,
     val width: Float,
     val height: Float,
+    val scaledWidth: Float,
+    val scaledHeight: Float,
     val contentScale: Float
 )
