5f01f76548c6263ccaf0c5f2188655c5357afe1e
sen <senxd@protonmail.com>
2022-07-10 11:01:55 -0700

    Font

--- src/test/kotlin/net/prismclient/aether/screens/TestingScreen.kt
+++ src/test/kotlin/net/prismclient/aether/screens/TestingScreen.kt
@@ -1,12 +1,14 @@
 package net.prismclient.aether.screens
 
 import examples.deps.Generic
+import net.prismclient.aether.ui.component.type.image.UIImageSheet
 import net.prismclient.aether.ui.component.type.layout.auto.UIAutoLayout
 import net.prismclient.aether.ui.component.type.layout.auto.UIAutoLayoutSheet
 import net.prismclient.aether.ui.component.type.layout.container.UIContainer
 import net.prismclient.aether.ui.component.type.layout.list.UIListLayout
 import net.prismclient.aether.ui.component.type.layout.styles.UIContainerSheet
 import net.prismclient.aether.ui.component.util.enums.UIAlignment
+import net.prismclient.aether.ui.dsl.UIAssetDSL
 import net.prismclient.aether.ui.screen.UIScreen
 import net.prismclient.aether.ui.style.UIStyleSheet
 import net.prismclient.aether.ui.util.*
@@ -48,44 +50,52 @@ class TestingScreen : UIScreen {
                 layoutPadding = paddingOf(8, 9, 8, 9)
             }
 
-            val navbar: UIContainer<UIContainerSheet> = component(UIContainer<UIContainerSheet>(null)) {
-                list(UIListLayout.ListDirection.Vertical, UIListLayout.ListOrder.Forward) {
-                    selectable<UIAutoLayout> {
-                        ignore { label("MENU", "navbar-title") }
-                        navButton("Dashboard", "ui")
-                        navButton("Settings", "ui")
-                        navButton("Store", "ui")
-                        navButton("Profiles", "ui")
-
-                        ignore { label("SOCIAL", "navbar-title") }
-                        navButton("Messages", "ui")
-                        navButton("Achievements", "ui")
-                        navButton("Friends", "ui")
-                        navButton("Recordings", "ui")
-
-                        onSelection {
-                            it.applyStyle("active-navbar-button")
-                        }
-
-                        onDeselection {
-                            it.applyStyle("navbar-button")
-                        }
-                        this.components.forEach { component ->
-                            component.onMousePressed {
-                                selectComponent(it as UIAutoLayout)
-                            }
-                        }
-                        this.selectComponent(0)
-                    }
-                }.style(UIContainerSheet("")) {
-                    y = px(118)
-                    size(rel(1), rel(0.8))
-                }
-                navbarButtonLayout = null
-            }.style(UIContainerSheet("navbar")) {
-                size(px(236), rel(1))
-                background(colorOf(36, 37, 37))
+            styleOf(UIImageSheet("a")) {
+                size(500, 500)
             }
+
+            UIAssetDSL.image("background", "/prism/backgrounds/main-menu.png")
+
+            image("background", "a")
+
+//            val navbar: UIContainer<UIContainerSheet> = component(UIContainer<UIContainerSheet>(null)) {
+//                list(UIListLayout.ListDirection.Vertical, UIListLayout.ListOrder.Forward) {
+//                    selectable<UIAutoLayout> {
+//                        ignore { label("MENU", "navbar-title") }
+//                        navButton("Dashboard", "ui")
+//                        navButton("Settings", "ui")
+//                        navButton("Store", "ui")
+//                        navButton("Profiles", "ui")
+//
+//                        ignore { label("SOCIAL", "navbar-title") }
+//                        navButton("Messages", "ui")
+//                        navButton("Achievements", "ui")
+//                        navButton("Friends", "ui")
+//                        navButton("Recordings", "ui")
+//
+//                        onSelection {
+//                            it.applyStyle("active-navbar-button")
+//                        }
+//
+//                        onDeselection {
+//                            it.applyStyle("navbar-button")
+//                        }
+//                        this.components.forEach { component ->
+//                            component.onMousePressed {
+//                                selectComponent(it as UIAutoLayout)
+//                            }
+//                        }
+//                        this.selectComponent(0)
+//                    }
+//                }.style(UIContainerSheet("")) {
+//                    y = px(118)
+//                    size(rel(1), rel(0.8))
+//                }
+//                navbarButtonLayout = null
+//            }.style(UIContainerSheet("navbar")) {
+//                size(px(236), rel(1))
+//                background(colorOf(36, 37, 37))
+//            }
         }
     }
 
